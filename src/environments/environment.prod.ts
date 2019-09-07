05f34573a58ed2cc2900109633e4c4f55ffce716
SATISH SINGH <satishsinghoct87@gmail.com>
2019-09-07 23:28:05 +0530

    Updated changes for heroku log build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig :{
-    apiKey: "AIzaSyAb-RQqpMKOvhaS4ScXwm5JSFcZiDlvNFY",
-    authDomain: "gothic-victor-151010.firebaseapp.com",
-    databaseURL: "https://gothic-victor-151010.firebaseio.com",
-    projectId: "gothic-victor-151010",
-    storageBucket: "gothic-victor-151010.appspot.com",
-    messagingSenderId: "438779511224",
-    appId: "1:438779511224:web:6aed0cee2e6238bd"
-  }
+  production: true
 };
