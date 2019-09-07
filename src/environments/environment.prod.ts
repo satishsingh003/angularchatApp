8dd5d9a1386fd5de0111047c45a12ad2944240fa
SATISH SINGH <satishsinghoct87@gmail.com>
2019-09-07 22:34:30 +0530

    Env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyAb-RQqpMKOvhaS4ScXwm5JSFcZiDlvNFY",
+    authDomain: "gothic-victor-151010.firebaseapp.com",
+    databaseURL: "https://gothic-victor-151010.firebaseio.com",
+    projectId: "gothic-victor-151010",
+    storageBucket: "gothic-victor-151010.appspot.com",
+    messagingSenderId: "438779511224",
+    appId: "1:438779511224:web:6aed0cee2e6238bd"
+  }
 };
