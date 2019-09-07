f22c394b2e27396438c5b9249304a79372e6ba35
SATISH SINGH <satishsinghoct87@gmail.com>
2019-09-07 22:28:39 +0530

    updates to deploy to heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig :{
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyAb-RQqpMKOvhaS4ScXwm5JSFcZiDlvNFY",
+    authDomain: "gothic-victor-151010.firebaseapp.com",
+    databaseURL: "https://gothic-victor-151010.firebaseio.com",
+    projectId: "gothic-victor-151010",
+    storageBucket: "gothic-victor-151010.appspot.com",
+    messagingSenderId: "438779511224",
+    appId: "1:438779511224:web:6aed0cee2e6238bd"
   }
 };
 
